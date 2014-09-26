6590425c1e41d652cd49c59a135681b81bf44be5
maxint <lnychina@gmail.com>
2014-09-26 10:30:30 +0800

    jni configuration for NDK r8e, android-9

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # Compiles fftw3 for Android
-# Make sure you have NDK_DIR defined in .bashrc or .bash_profile
 
 #NDK Version r9c  http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86_64.tar.bz2
-NDK_DIR="/home/hekai/software/android-ndk-r9c"
+NDK_DIR="/home/NDK/android-ndk-r8e"
 INSTALL_DIR="`pwd`/jni/fftw3"
-SRC_DIR="`pwd`/../fftw-3.3.3"
+SRC_DIR="`pwd`/../fftw-3.3.4"
 
 cd $SRC_DIR
 
-export PATH="$NDK_DIR/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/:$PATH"
-export SYS_ROOT="$NDK_DIR/platforms/android-8/arch-arm/"
+export PATH="$NDK_DIR/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86/bin/:$PATH"
+export SYS_ROOT="$NDK_DIR/platforms/android-9/arch-arm/"
 export CC="arm-linux-androideabi-gcc --sysroot=$SYS_ROOT -march=armv7-a -mfloat-abi=softfp"
 export LD="arm-linux-androideabi-ld"
 export AR="arm-linux-androideabi-ar"
@@ -22,13 +21,14 @@ export STRIP="arm-linux-androideabi-strip"
 mkdir -p $INSTALL_DIR
 ./configure --host=arm-eabi \
 	--prefix=$INSTALL_DIR \
-	LIBS="-lc -lgcc" \
 	--enable-float \
 	--enable-threads \
-#	--with-combined-threads \
+    #--with-combined-threads \
 	--enable-neon
 
-make
-make install
+make clean
+make -j4 install
+
+cd -
 
 exit 0
---
+++ jni/Application.mk
@@ -0,0 +1,9 @@
+# The ARMv7 is significanly faster due to the use of the hardware FPU
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-9
+#APP_OPTIM := debug
+#APP_STL := gnustl_shared
+#APP_STL := gabi++_static
+#APP_CFLAGS := -std=c++11
+
+NDK_TOOLCHAIN_VERSION := 4.7
